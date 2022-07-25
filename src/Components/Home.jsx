2984518df1c8d318b366a039a9da431b29085825
Exdevo <95994620+Exdevo@users.noreply.github.com>
2022-07-25 14:12:06 +0000

    Add files via upload
    You can play around with the other photos uploaded .

---
+++ src/Components/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='hero'>
+        <div class="card bg-dark text-white border-0">
+  <img src="/images/code.jpg" class="card-img" alt="background" height="550px"/>
+  <div class="card-img-overlay d-flex  flex-column ">
+      <div className="container">
+    <h5 class="card-title display-3 fw-bolder mb-0">Koech's Application!</h5>
+    <p class="card-text">Follow me for more stuff like this.</p>
+    <p class="card-text">Last updated 3 mins ago</p>
+    <ul>
+      <li>React is composable</li>
+      <li>REact is declarative</li>
+      <li>React has strong community</li>
+      <li>React was built by facebook</li>
+    </ul>
+      </div>
+  </div>
+</div>
+        </div>
+  )
+}
+
